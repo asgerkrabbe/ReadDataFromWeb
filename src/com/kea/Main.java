b493b91149c99d1b206d5ca9a13c7f0d872c1eed
asgerkrabbe <asgerkrabbe@gmail.com>
2020-10-29 16:39:20 +0100

    update

--- src/com/kea/Main.java
+++ src/com/kea/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         // Opretter ArrayList til at indeholde listen med rates i
-        ArrayList<Rateinfo> rateinfos=new ArrayList<Rateinfo>();
+        ArrayList<Rateinfo> rateinfos = new ArrayList<Rateinfo>();
 
         //Instantiating the URL class
         URL url = new URL("https://www.nationalbanken.dk/_vti_bin/DN/DataService.svc/CurrencyRatesXML?lang=da");
@@ -18,15 +18,15 @@ public class Main {
         //Instantiating the StringBuffer class to hold the result
         //StringBuffer sb = new StringBuffer();
         // Tælle variabel i til at tælle med
-        int i=0;
+        int i = 0;
         //  Anvendes til at holde code
-        String Code="";
+        String Code = "";
         // Bruges til at indeholde desc
-        String Desc="";
+        String Desc = "";
         // Indeholder rate
-        String Rate="";
+        String Rate = "";
 
-        while(sc.hasNext()) {
+        while (sc.hasNext()) {
             String t = sc.next();
             //sb.append(t);
             i++;
@@ -58,22 +58,20 @@ public class Main {
                 }
             }
         }
-                for (int j = 0; j < rateinfos.size();j++)
-                {
-                    System.out.println(""+rateinfos.get(j));
-                }
+        for (int j = 0; j < rateinfos.size(); j++) {
+            System.out.println("" + rateinfos.get(j));
+        }
 
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Input code name: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input code name: ");
 
-                String valutaType=scanner.next();
+        String valutaType = scanner.next();
 
-                for (int j = 0; j < rateinfos.size(); j++) {
-                    if(rateinfos.get(j).getCode().contains(valutaType))
-                    {
-                        System.out.println(""+rateinfos.get(j));
-                        System.out.println();
-                    }
-                }
+        for (int j = 0; j < rateinfos.size(); j++) {
+            if (rateinfos.get(j).getCode().contains(valutaType)) {
+                System.out.println("" + rateinfos.get(j));
+                System.out.println();
+            }
+        }
     }
 }
